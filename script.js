faa4f9ffa363ada3753c6c0c7007f3c60556fafb
YOUR <jodiechacreton@gmail.com>
2026-06-12 16:37:11 -0400

    buttons

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 // Declare variables below to save the different divs of your story.
 
-let Button_1= document.querySelector ('.boyButton-1');
-let Button_2= document.querySelector ('.girlButton-1');
+let Button_1= document.querySelector('.boyButton-1');
+let Button_2= document.querySelector('.girlButton-2');
+let boyBackpack = document.querySelector('.boyBackpack')
+let optionOneScreen = document.querySelector('.option-one-screen');
+let opening = document.querySelector('.opening');
+let optionTwoScreen = document.querySelector('.option-two-screen');
 
+Button_1.addEventListener('click', function() {
+    opening.style.display = "none";
+    optionOneScreen.style.display = "block";
+});
 
-let optionOneScreen = document.querySelector('option-one-screen');
-let opening = document.querySelector('story-opening');
-optionOneBtn. addEventListener('click', function() {
-    optionOneScreen.style.display = 'block'; 
-    opening.style.display = 'none';
-}) ;
+Button_2.addEventListener('click', function(){
+    opening.style.display = "none";
+    optionTwoScreen.style.display = "block";
+});
 
 
 
@@ -20,11 +26,6 @@ optionOneBtn. addEventListener('click', function() {
 // When you're ready to make event handlers, uncomment the code below. 
 //  - Then fill in the blanks with the correct variables.
 
-
-Button_1.addEventListener('click', function(){
-
-
-});
 
 // INSERT_VARIABLE.addEventListener('click', function(){
 
