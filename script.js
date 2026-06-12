077b824732d08ceb4124cf520c4b867aab8eb6fc
YOUR <jodiechacreton@gmail.com>
2026-06-12 13:36:51 -0400

    started

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // Declare variables below to save the different divs of your story.
 
+let Button_1= document.querySelector ('.boyButton-1');
+let Button_2= document.querySelector ('.girlButton-1');
 
+
+let optionOneScreen = document.querySelector('option-one-screen');
+let opening = document.querySelector('story-opening');
+optionOneBtn. addEventListener('click', function() {
+    optionOneScreen.style.display = 'block'; 
+    opening.style.display = 'none';
+}) ;
 
 
 
@@ -12,9 +21,10 @@
 //  - Then fill in the blanks with the correct variables.
 
 
-// INSERT_VARIABLE.addEventListener('click', function(){
+Button_1.addEventListener('click', function(){
 
-// });
+
+});
 
 // INSERT_VARIABLE.addEventListener('click', function(){
 
