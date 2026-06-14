a0e20a4cd806525da27fd892e62b1e0f92c77ae3
YOUR <jodiechacreton@gmail.com>
2026-06-13 20:13:17 -0400

    added and fixed alot

--- script.js
+++ script.js
@@ -2,15 +2,74 @@
 
 let Button_1= document.querySelector('.boyButton-1');
 let Button_2= document.querySelector('.girlButton-2');
-let boyBackpack = document.querySelector('.boyBackpack')
+let Button_3= document.querySelector('.button-3');
+let Button_4= document.querySelector('.button-4');
+let Button_5= document.querySelector('.button-5');
+let Button_6= document.querySelector('.button-6');
+let Button_7= document.querySelector('.button-7');
+let Button_8= document.querySelector('.button-8');
+let Magical_Pencil = document.querySelector('.magical-pencil');
+let Results= document.querySelector('.results');
+let Play_Again= document.querySelector('.play-again')
+
+
+let resultPage = document.querySelector ('.results-page')
+
+let Red = document.querySelector('.red');
+
+let Blue = document.querySelector('.blue');
+
 let optionOneScreen = document.querySelector('.option-one-screen');
+
 let opening = document.querySelector('.opening');
+
+let startWriting = document.querySelector('.start-writing');
+
 let optionTwoScreen = document.querySelector('.option-two-screen');
+
+
+
+Magical_Pencil.addEventListener('click', function(){
+let name = prompt("What are you giving up?");
+alert("You wrote: " + answer); 
+});
+
+
+
+
+Results.addEventListener('click', function() {
+    startWriting.style.display = "none";
+    resultPage.style.display = "block";
+});
+
+
+Play_Again.addEventListener('click', function() {
+    resultPage.style.display = "none";
+    opening.style.display = "block";
+});
+
+Play_Again.addEventListener('click', function() {
+    Red.style.display = "none";
+    opening.style.display = "block";
+});
+
 
 Button_1.addEventListener('click', function() {
     opening.style.display = "none";
     optionOneScreen.style.display = "block";
 });
+
+Button_3.addEventListener('click', function(){
+    optionOneScreen.style.display = "none";
+    Red.style.display = "block";
+});
+
+
+Button_4.addEventListener('click', function(){
+    optionOneScreen.style.display = "none";
+    Blue.style.display = "block";
+});
+
 
 Button_2.addEventListener('click', function(){
     opening.style.display = "none";
@@ -18,10 +77,16 @@ Button_2.addEventListener('click', function(){
 });
 
 
+Button_7.addEventListener('click', function(){
+    Blue.style.display = "none";
+    opening.style.display = "block";
+});
 
 
-
-
+Button_8.addEventListener('click', function(){
+    Blue.style.display = "none";
+    startWriting.style.display = "block";
+});
 
 // When you're ready to make event handlers, uncomment the code below. 
 //  - Then fill in the blanks with the correct variables.
